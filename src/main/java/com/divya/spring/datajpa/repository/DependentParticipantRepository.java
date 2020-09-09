a7d1ec0149365dbbcbd9764b04be704bcc24ac43
Divya dharanikota <divya.dharanikota12@gmail.com>
2020-09-09 18:40:32 -0500

    updated repository dependent

--- src/main/java/com/divya/spring/datajpa/repository/DependentParticipantRepository.java
+++ src/main/java/com/divya/spring/datajpa/repository/DependentParticipantRepository.java
@@ -6,9 +6,8 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.divya.spring.datajpa.model.DependentParticipant;
-import com.divya.spring.datajpa.model.Participant;
 
 public interface DependentParticipantRepository extends JpaRepository<DependentParticipant, Long> {
-	List<Participant> findByDateOfBirth(Date dateOfBirth);
-	List<Participant> findByFirstNameContaining(String firstName);
+	List<DependentParticipant> findByDateOfBirth(Date dateOfBirth);
+	List<DependentParticipant> findByFirstNameContaining(String firstName);
 }
